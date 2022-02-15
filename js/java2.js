fd81657dfacfaed9d179c1c598967d88b9b65ccb
AndersonPonced <89653471+AndersonPonced@users.noreply.github.com>
2022-02-15 16:37:43 -0300

    Add files via upload

---
+++ js/java2.js
@@ -0,0 +1,10 @@
+const card1 = document.querySelector(".card1")
+const card2 = document.querySelector(".card2")
+
+card1.addEventListener('click', ()=>{
+  alert("Esta fuera de servicio");
+})
+
+card2.addEventListener('click', ()=>{
+  alert("Esta fuera de servicio");
+})
